43a9e30920d700b6e77ffab756960acbd84ead73
francescofilitti <79720164+francescofilitti@users.noreply.github.com>
2021-03-14 18:56:44 +0100

    commit 4
    Controller ed implementazione metodi nel service impl #4

--- server-Crud2-Gruppo4-/crud-gruppo-quattro/src/main/java/controller/CrudController.java
+++ server-Crud2-Gruppo4-/crud-gruppo-quattro/src/main/java/controller/CrudController.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
-/**
- *
- * @author Valerio
- */
+import dto.CarrelloDto;
+import dto.CriterioRicercaDto;
+import dto.ProdottoDto;
+import model.Prodotto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.CrudService;
+
+@CrossOrigin("*")
+@RestController
 public class CrudController {
-    
+
+    @Autowired
+    CrudService crudService;
+
+    @RequestMapping("/aggiungi")
+    public CarrelloDto aggiungi(@RequestBody ProdottoDto dto) {
+
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    //dubbio se passare un prodottoDto o criterio ricercadto
+    @RequestMapping("/modifica")
+    public CarrelloDto modifica(@RequestBody ProdottoDto dto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @RequestMapping("/rimuovi")
+    public CarrelloDto rimuovi(@RequestBody ProdottoDto dto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @RequestMapping("/aggiorna")
+    public CarrelloDto aggiorna() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @RequestMapping("/ricerca")
+    public CarrelloDto ricerca(@RequestBody CriterioRicercaDto dto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
