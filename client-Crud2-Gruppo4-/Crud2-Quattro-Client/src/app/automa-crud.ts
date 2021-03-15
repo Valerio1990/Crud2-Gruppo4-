dc48da2f1efadc56d2a7cb42ff14fe0af7f73b71
AlbertoMartinezMontero <79788629+AlbertoMartinezMontero@users.noreply.github.com>
2021-03-15 12:05:43 +0100

    commit #6
    Implementazione automa-crud lato client #9

--- client-Crud2-Gruppo4-/Crud2-Quattro-Client/src/app/automa-crud.ts
+++ client-Crud2-Gruppo4-/Crud2-Quattro-Client/src/app/automa-crud.ts
@@ -1,1 +1,124 @@
-export class AuromaCrud implements State{}
+module test{
+export class AutoCrud implements State{
+
+
+
+public statoAttuale: State;
+constructor(){
+
+}
+public setState(state: State){
+this.statoAttuale: State;
+}
+
+public getState():{
+return this.currentState
+}
+}
+
+export class RicercaSate implements State{
+
+utomaP: AuromaCrud;
+
+constructor(automaCrud: AuromaCrud){
+this.automaP = automaP
+}
+this.next(e:Event):void{
+  if (e instanceof AddEvent){
+  this.automaP.setState(this.automaP.aggiungiState);
+  }
+  else(e instanceof SelezionaEvent){
+   this.automaP.setState(this.automaP.VisualizzaState);
+
+  }
+  else { console.log("evento inatteso!")}
+}
+}
+
+
+export class VisualizzaState implements State{
+
+utomaP: AuromaCrud;
+
+constructor(automaCrud: AuromaCrud){
+this.automaP = automaP;
+}
+this.next(e:Event):void{
+  if (e instanceof AddEvent){
+     this.automaP.setState(this.automaP.AggiungiState);
+  }
+
+  if(e instanceof ModificaEvent){
+       this.automaP.setState(this.automaP.ModificaState);
+  }
+  if(e instanceof RimuoviEvent){
+       this.automaP.setState(this.automaP.RimuoviState);
+  }
+  if(e instanceof RicercaEvent){
+   this.automaP.setState(this.automaP.RicercaSate);
+  }
+ if(e instanceof SelezionaEvent){
+       this.automaP.setState(this.automaP.VisualizzaState);
+  }
+    else { console.log("evento inatteso!")}
+}
+}
+
+export class ModificaState implements State{
+
+utomaP: AuromaCrud;
+
+constructor(automaCrud: AuromaCrud){
+this.automaP = automaP
+}
+this.next(e:Event):void{
+  if (e instanceof AnnullaEvent){
+   this.automaP.setState(this.automaP.VisualizzaState);
+
+  else if(e instanceof ConfermaEvent){
+    this.automaP.setState(this.automaP.VisualizzaState);
+  }
+  
+  else { console.log("evento inatteso!")}
+}
+}
+export class RimuoviState implements State{
+
+utomaP: AuromaCrud;
+
+constructor(automaCrud: AuromaCrud){
+this.automaP = automaP
+}
+this.next(e:Event):void{
+  if (e instanceof ConfermaEvent){
+ this.automaP.setState(this.automaP.RicercaState);
+  }
+
+  else if(e instanceof AnnullaEvent){
+   this.automaP.setState(this.automaP.VisualizzaState);
+  }
+  
+  else { console.log("evento inatteso!")}
+}
+}
+
+export class AggiungiState implements State{
+
+automaP: AuromaCrud;
+
+constructor(automaCrud: AuromaCrud){
+this.automaP = automaP
+}
+this.next(e:Event):void{
+  if (e instanceof AnnullaEvent){
+    this.automaP.setState(this.automaP.RicercaSate);
+  }
+
+  else if(e instanceof ConfermaEvent){
+    this.automaP.setState(this.automaP.VisualizzaState);
+  }
+
+  else { console.log("evento inatteso!")}
+}
+}
+}
--- client-Crud2-Gruppo4-/Crud2-Quattro-Client/src/app/seleziona-event.ts
+++
@@ -1,1 +0,0 @@
-export  class SelezionaEvent  implements Event{}
